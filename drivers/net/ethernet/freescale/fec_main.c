1f38d716ae6e3102d2a9aebf8ae7e145d38835cb
Stefan Wahren <stefan.wahren@i2se.com>
2016-03-23 21:09:01 +0000

    net: fec: Reset ethernet PHY whenever the enet_out clock is  being enabled
    If a PHY uses ENET_OUT as reference clock, it may need a RESET to get
    functional after the clock had been disabled.
    
    Failure to do this results in the link state constantly toggling
    between up and down:
    fec 02188000.ethernet eth0: Link is Up - 100Mbps/Full - flow control rx/tx
    fec 02188000.ethernet eth0: Link is Down
    fec 02188000.ethernet eth0: Link is Up - 100Mbps/Full - flow control rx/tx
    fec 02188000.ethernet eth0: Link is Down
    [...]
    
    Signed-off-by: Stefan Wahren <stefan.wahren@i2se.com>

--- drivers/net/ethernet/freescale/fec_main.c
+++ drivers/net/ethernet/freescale/fec_main.c
@@ -67,6 +67,7 @@
 
 static void set_multicast_list(struct net_device *ndev);
 static void fec_enet_itr_coal_init(struct net_device *ndev);
+static void fec_reset_phy(struct platform_device *pdev);
 
 #define DRIVER_NAME	"fec"
 
@@ -1870,6 +1871,8 @@ static int fec_enet_clk_enable(struct net_device *ndev, bool enable)
 			ret = clk_prepare_enable(fep->clk_enet_out);
 			if (ret)
 				goto failed_clk_enet_out;
+
+			fec_reset_phy(fep->pdev);
 		}
 		if (fep->clk_ptp) {
 			mutex_lock(&fep->ptp_clk_mutex);
@@ -3228,9 +3231,10 @@ static int fec_enet_init(struct net_device *ndev)
 #ifdef CONFIG_OF
 static void fec_reset_phy(struct platform_device *pdev)
 {
-	int err, phy_reset;
 	int msec = 1;
 	struct device_node *np = pdev->dev.of_node;
+	struct net_device *ndev = platform_get_drvdata(pdev);
+	struct fec_enet_private *fep = netdev_priv(ndev);
 
 	if (!np)
 		return;
@@ -3240,18 +3244,9 @@ static void fec_reset_phy(struct platform_device *pdev)
 	if (msec > 1000)
 		msec = 1;
 
-	phy_reset = of_get_named_gpio(np, "phy-reset-gpios", 0);
-	if (!gpio_is_valid(phy_reset))
-		return;
-
-	err = devm_gpio_request_one(&pdev->dev, phy_reset,
-				    GPIOF_OUT_INIT_LOW, "phy-reset");
-	if (err) {
-		dev_err(&pdev->dev, "failed to get phy-reset-gpios: %d\n", err);
-		return;
-	}
+	gpio_set_value_cansleep(fep->phy_reset, 0);
 	msleep(msec);
-	gpio_set_value_cansleep(phy_reset, 1);
+	gpio_set_value_cansleep(fep->phy_reset, 1);
 }
 #else /* CONFIG_OF */
 static void fec_reset_phy(struct platform_device *pdev)
@@ -3436,6 +3431,17 @@ fec_probe(struct platform_device *pdev)
 	pm_runtime_get_noresume(&pdev->dev);
 	pm_runtime_set_active(&pdev->dev);
 	pm_runtime_enable(&pdev->dev);
+
+	if (np) {
+		fep->phy_reset = of_get_named_gpio(np, "phy-reset-gpios", 0);
+
+		if (gpio_is_valid(fep->phy_reset)) {
+			ret = devm_gpio_request_one(&pdev->dev, fep->phy_reset,
+						    GPIOF_OUT_INIT_LOW, "phy-reset");
+			if (ret)
+				dev_err(&pdev->dev, "failed to get phy-reset-gpios: %d\n", ret);
+		}
+	}
 
 	fec_reset_phy(pdev);
 
